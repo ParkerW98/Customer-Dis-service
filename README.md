ce81705f41857e2b02d66c3cef2961491184032a
Parker Williams <parker.business.1998@gmail.com>
2023-04-20 14:52:22 -0700

    math change function + terrible styling

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Customer-Dis-service
 An assignment for my BIT176 course
+
+Asks users to try and solve a math problem that continually changes every time they focus on the text entry box or enter/remove a number.
+
+Successfully infuriating!
--- docs/index.js
+++ docs/index.js
@@ -1,18 +1,51 @@
-const mathProblem = "4 + 4 - 8 * 10 + 1";
-
-function changeMath() {
-
-let item2 = 10;
-
-document.getElementById("math-problem").innerHTML = ("1 + "
-+ item2 +
-" * 8 - 4 + 4");
-
-//onkeyup.item2 = item2 / 2;
-
-}
-
 function revertMath() {
 
     document.getElementById("math-problem").innerHTML = ("4 + 4 - 8 * 10 + 1");
 }
+
+function convertNumber() {
+    const inputString = "4 + 4 - 8 * 10 + 1";
+    let numbersArray = inputString.match(/-?\d+(\.\d+)?/g).map(Number);
+    const operatorsArray = inputString.match(/[\+\-\*\/]/g);
+
+    // choose a random number from the array to modify
+    const randomIndex = Math.floor(Math.random() * numbersArray.length);
+    const originalNumber = numbersArray[randomIndex];
+
+    // modify the number
+    const newNumber = originalNumber + 10;
+    numbersArray[randomIndex] = newNumber;
+
+    // shuffle the array
+    for (let i = numbersArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+        [numbersArray[i], numbersArray[j]] = [numbersArray[j], numbersArray[i]];
+    }
+
+    // combine the shuffled array and operators array into a new string
+    let outputString = '';
+    for (let i = 0; i < numbersArray.length; i++) {
+        outputString += numbersArray[i];
+    if (i < operatorsArray.length) {
+        outputString += ' ' + operatorsArray[i] + ' ';
+    }
+    }
+
+    document.getElementById("math-problem").innerHTML = outputString;
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('myForm');
+    form.addEventListener('submit', (event) => {
+      event.preventDefault(); // prevent form from submitting and reloading the page
+        const inputFieldValue = document.getElementById('inputField').value;
+
+        if (inputFieldValue == 69) {
+            alert("You're funny, I like you");
+        } else
+        alert('WRONG!!!! What are you? An Idiot Sandwich!?');
+        document.getElementById('myForm').innerHTML =
+        "<img src='assets/gordon-ramsay-what-are-you.gif' alt='gordon gif' height='500px' width='800px'>";
+    });
+});
